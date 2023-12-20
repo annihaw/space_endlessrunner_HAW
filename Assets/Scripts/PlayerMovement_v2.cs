240297d92234fc62d398fac0aeced73418891f3d
annihaw <154251374+annihaw@users.noreply.github.com>
2023-12-20 18:56:41 +0100

    PlayerMovement Script an Kinect angepasst
    Die Kinect Daten wurden im PlayerMovement hinzugefügt und der erste Test hat funktioniert.
    Der Spieler kann sich mit der Kinect nach links, rechts und oben bewegen.
    
    Nachteil mit Kinect: Es ist sehr sehr verzögert!! Dringende Ausbesserung notwendig, Logik ist aber da.
    
    Spiel funktioniert auch ohne dass die Kinect angeschlossen ist mit den üblichen Tastenkombinationen.

--- Assets/Scripts/PlayerMovement_v2.cs
+++ Assets/Scripts/PlayerMovement_v2.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Windows.Kinect;
 
 public class PlayerMovement_v2 : MonoBehaviour
 {
+
     Rigidbody rb;
     [SerializeField] float LRmovementSpeed = 5f;
     [SerializeField] float VHmovementSpeed = 0f;                     // Vorne / Hinten Movement Speed
@@ -11,19 +13,52 @@ public class PlayerMovement_v2 : MonoBehaviour
 
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask ground;
-
+    [SerializeField]
     public GameObject playerObject;
 
+    private KinectSensor _sensor;
+    private BodyFrameReader _reader;
+    private Body[] _Data;
 
-    
+    // Variablen fuer die Gelenkpositionen - speichert immer die vorherigen Positionen
+    private float _previousXPosition;
+    private float _previousYPositionBase;
+    private float _previousHeadPositionY; 
+    private float _previousFootLeftPositionY;
+    private float _previousFootRightPositionY;
 
-    
+    // Schwellen fuer die Positionsaenderung - umso gruesser bzw. kleiner diese sind, desto schwieriger
+    private float MovementThreshold = 0.1f;     // Schwelle fuer Seitwaertsbewegung
+    private float BendingThreshold = -0.2f;     // Schwelle fuer das Buecken (negative Werte, da Y-Position nach unten zunimmt)
+    public float JumpThreshold = 0.1f;          // Schwelle fuer das Erkennen eines Sprungs
+
+    // Variablen fuer den aktuellen Zustand
+    public static bool isJumping = false;
+    public static bool isMovingLeft = false;
+    public static bool isMovingRight = false;
+    public static bool isDucking = false; 
+
+    // Start wird beim ersten Frame aufgerufen
     void Start()
     {
-        rb = GetComponent<Rigidbody>();     // Get the Rigidbody component from the player GameObject
+        rb = GetComponent<Rigidbody>();
+
+        _sensor = KinectSensor.GetDefault();
+
+        if (_sensor != null)
+        {
+            _reader = _sensor.BodyFrameSource.OpenReader();
+
+            if (!_sensor.IsOpen)
+            {
+                _sensor.Open();
+            }
+        }
+
+        _Data = new Body[_sensor.BodyFrameSource.BodyCount];
     }
 
-    // Update is called once per frame
+    // Update wird einmal pro Frame aufgerufen
     void Update()
     {
         //float horizontalInput = Input.GetAxis("Horizontal");
@@ -31,33 +66,52 @@ public class PlayerMovement_v2 : MonoBehaviour
         // Preserve the vertical velocity
         float verticalVelocity = rb.velocity.y;
 
+        if (_reader != null)
+        {
+            var frame = _reader.AcquireLatestFrame();
+
+            if (frame != null)
+            {
+                frame.GetAndRefreshBodyData(_Data);
+
+                foreach (var body in _Data)
+                {
+                    if (body != null && body.IsTracked)
+                    {
+                        horizontalMovement(body);
+                        verticalMovement(body);
+                    }
+                }
+
+                frame.Dispose();
+            }
+        }
+
         // Reset horizontal velocity when neither left nor right key is pressed
-        if (!Input.GetKey("left") && !Input.GetKey("right"))
+        if ((!Input.GetKey("left") && !Input.GetKey("right")) || (!isMovingRight && !isMovingLeft))
         {
             rb.velocity = new Vector3(rb.velocity.x * 0.9f, verticalVelocity, VHmovementSpeed);
         }
 
        // rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, VHmovementSpeed);
         
-        if(Input.GetKey("left"))
+        if(Input.GetKey("left") || isMovingLeft)
         {
             GoLeft();
         }
 
-        if(Input.GetKey("right"))
+        if(Input.GetKey("right") || isMovingRight)
         {
             GoRight();
         }
 
-
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if ((Input.GetButtonDown("Jump") || isJumping) && IsGrounded())
         {
             Jump();
         }
 
     }
     
-
     void GoLeft()
     {
         rb.velocity = new Vector3(LRmovementSpeed * -1, rb.velocity.y, rb.velocity.z);
@@ -81,9 +135,6 @@ public class PlayerMovement_v2 : MonoBehaviour
         */
         
     }
-
-
-
     void Jump()
     {
         rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
@@ -91,12 +142,124 @@ public class PlayerMovement_v2 : MonoBehaviour
     }
 
 
-   
-    
-    
     bool IsGrounded()
     {
        return Physics.CheckSphere(groundCheck.position, .1f, ground);
     } 
-    
+
+
+   // Ueberwacht die links und rechts Bewegung
+    void horizontalMovement(Body body)
+    {
+        // Definiere den Gelenktypen fuer den SpineBase (Rumpf)
+        JointType spineBase = JointType.SpineBase;
+        // Erhalte die Positionen des Gelenkes des Koerpers aus dem Kinect-Body-Objekt
+        CameraSpacePoint basePosition = body.Joints[spineBase].Position;
+        // aktuelle X-Position des Gelenks
+        float currentXPosition = basePosition.X;
+
+         // Ueberpruefen ob die Aenderung der X-Position groeser ist als der Schwellenwert fuer seitliche Bewegungen
+        if (Mathf.Abs(currentXPosition - _previousXPosition) > MovementThreshold)
+        {
+            // Falls die X-Position zunimmt, setze den Bewegungsstatus nach rechts.
+            if (currentXPosition - _previousXPosition > 0)
+            {
+                isMovingRight = true;
+                isMovingLeft = false;
+                Debug.Log("Bewegt sich nach rechts------------------------------------------!");
+            }
+            // Falls die X-Position abnimmt, setze den Bewegungsstatus nach links.
+            else
+            {
+                isMovingLeft = true;
+                isMovingRight = false;
+                Debug.Log("Bewegt sich nach links----");
+            }
+        }
+        // Wenn die Aenderung der X-Position unter dem Schwellenwert liegt, setze beide Bewegungsstatus zurueck.
+        else
+        {
+            isMovingLeft = false;
+            isMovingRight = false;
+        }
+
+        _previousXPosition = currentXPosition;
+    }
+
+    /// Ueberwacht das Springen und Ducken
+    void verticalMovement(Body body)
+    {
+        // Definiere die Gelenktypen fuer den SpineBase (Rumpf), Kopf, linken Fuss und rechten Fuss
+        JointType spineBase = JointType.SpineBase;
+        JointType head = JointType.Head;
+        JointType footLeft = JointType.FootLeft;
+        JointType footRight = JointType.FootRight;
+
+        // Erhalte die Positionen der Gelenke des Koerpers aus dem Kinect-Body-Objekt
+        CameraSpacePoint basePosition = body.Joints[spineBase].Position;
+        CameraSpacePoint headPosition = body.Joints[head].Position;
+        CameraSpacePoint footLeftPosition = body.Joints[footLeft].Position;
+        CameraSpacePoint footRightPosition = body.Joints[footRight].Position;
+
+        // aktuelle Y-Positionen der verschiedenen Gelenke
+        float currentYPositionBase = basePosition.Y;
+        float currentYPositionHead = headPosition.Y;
+        float currentYPositionFootLeft = footLeftPosition.Y;
+        float currentYPositionFootRight = footRightPosition.Y;
+
+        // Variablen fuer die Differenzen der Positionen
+        float baseChange = Mathf.Abs(currentYPositionBase - _previousYPositionBase);
+        float headChange = currentYPositionHead - _previousHeadPositionY;
+        float footLeftChange = Mathf.Abs(currentYPositionFootLeft - _previousFootLeftPositionY);
+        float footRightChange = Mathf.Abs(currentYPositionFootRight - _previousFootRightPositionY);
+
+        // Ueberprueft ob sich die Y-Position des SpineBase-Gelenks oder der Fuesse ausreichend aendert
+        if (baseChange > JumpThreshold &&
+            footLeftChange > 0.05f &&
+            footRightChange > 0.05f )
+        {
+            // Hier wird der Sprungstatus geaendert
+            isJumping = true;
+            isDucking = false;
+            Debug.Log("Springt------------------------------------------------------------");
+        }
+        // Ueberpruefe, ob sich die Y-Position des Kopfes nach unten aendert
+        else if (headChange < BendingThreshold )
+        {
+            // Hier wird der Ducken-Status geaendert
+            isDucking = true;
+            isJumping = false;
+            Debug.Log("Duckt--");
+        }
+        else
+        {
+            // Wenn keine der Bedingungen erfuellt ist, werden beide Status zurueckgesetzt
+            isJumping = false;
+            isDucking = false;
+        }
+
+        // Speichere die aktuellen Y-Positionen fuer den naechsten Frame
+        _previousYPositionBase = currentYPositionBase;
+        _previousHeadPositionY = currentYPositionHead;
+        _previousFootLeftPositionY = currentYPositionFootLeft;
+        _previousFootRightPositionY = currentYPositionFootRight;
+    }
+
+    // Wird aufgerufen, wenn die Anwendung beendet wird
+    void OnApplicationQuit()
+    {
+        if (_reader != null)
+        {
+            _reader.Dispose();
+            _reader = null;
+        }
+
+        if (_sensor != null)
+        {
+            if (_sensor.IsOpen)
+            {
+                _sensor.Close();
+            }
+        }
+    }
 }
